bf9b109e032cb30d17f45348299c2064aa910439
Sebastian@gmail.com <10534@student.puzim.edu.pl>
2021-11-29 12:50:21 +0100

    Add function for matrix

--- Program.cs
+++ Program.cs
@@ -7,6 +7,17 @@ namespace mprog_lab_01
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            int [,] tablica = {{1,2}, {2,4}, {6,1}, {6,10}};
+            wrtieMatrix(tablica);
+        }
+
+        static void wrtieMatrix(int [,] matrix){
+            for (int i = 0; i < matrix.GetLength(0); i++){
+                for (int j = 0; j < matrix.GetLength(1); j++){
+                    Console.WriteLine($"Element [{i},{j}]: {matrix[i,j]}");
+                }
+            }
         }
     }
 }
